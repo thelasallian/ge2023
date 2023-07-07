35cc06dc2d818edd426b2c96d62adc7d1b80c715
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-07-07 15:42:42 +0800

    Added Google Fonts

--- php/modules/all-pages/head.php
+++ php/modules/all-pages/head.php
@@ -20,7 +20,10 @@
 <!-- Bootstrap CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
-<!-- TODO: Include Google Fonts <link> tags -->
+<!-- Google Fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,700;1,400;1,700&family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
 
 <!-- Common Stylesheet for All Pages of the Website -->
 <link rel="stylesheet" href="css/all-pages.css">
