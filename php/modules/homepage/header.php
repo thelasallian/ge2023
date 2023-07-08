b6f60305c9161b9c9ca8137c7ed037ccee34256c
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-07-08 17:07:22 +0800

    Responsive cover, without buttons

--- php/modules/homepage/header.php
+++ php/modules/homepage/header.php
@@ -1,16 +1,21 @@
-<div class="header-wrapper">
-    <header>
-        <!-- Logo -->
-        <div class="logo-wrapper">
-            
+<header>
+    <!-- Logo -->
+    <div class="logo-wrapper">
+        <img class="header-logo" src="assets/all-pages/tls-horizontal-logo-white.svg" alt="The LaSallian Logo">
+        
+    </div>
+    <!-- Art -->
+    <div class="cover-art-wrapper">
+        <div class="faux-cover-art-img"></div>    
+        <img class="cover-art-img" src="assets/homepage/cover/art-transparentbg.png" alt="">
+    </div>
+    <!-- Text -->
+    <div class="header-text-wrapper">
+        <h1 class="header-text-title display-1">On different pages</h1>
+        <h2 class="header-text-subtitle">2023 General Elections Special</h2>
+        <div class="header-buttons-wrapper">
+            Test
         </div>
-        <!-- Art -->
-        <div class="cover-art-wrapper">
-
-        </div>
-        <!-- Text -->
-        <div class="header-text-wrapper">
-
-        </div>
-    </header>
-</div>
+        
+    </div>
+</header>
