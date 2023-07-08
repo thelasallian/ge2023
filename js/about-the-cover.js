73ac92e330d0d0a82444f4b31c7f6c1423ac10fe
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-07-08 18:05:30 +0800

    Function to hide about the cover

--- js/about-the-cover.js
+++ js/about-the-cover.js
@@ -5,5 +5,15 @@ function displayAboutTheCover() {
 
     logo.style.display = "none";
     text.style.display = "none";
-    aboutTheCover.style.display = "block";
+    aboutTheCover.style.display = "flex";
+}
+
+function hideAboutTheCover() {
+    var logo = document.getElementById("logo-wrapper");
+    var text = document.getElementById("header-text-wrapper");
+    var aboutTheCover = document.getElementById("about-the-cover");
+
+    logo.style.display = "block";
+    text.style.display = "block";
+    aboutTheCover.style.display = "none";
 }
