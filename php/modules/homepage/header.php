245db461eb5b144850a6edba78dac4677ee8f344
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-07-08 17:41:05 +0800

    Fixed media queries that csscomb broke

--- php/modules/homepage/header.php
+++ php/modules/homepage/header.php
@@ -1,6 +1,10 @@
 <header>
+    <!-- <div id="about-the-cover">
+        <h1>About the Cover</h1>
+        <p>A communi observantia non est recedendum. Vivamus sagittis lacus vel augue laoreet rutrum faucibus. Nihilne te nocturnum praesidium Palati, nihil urbis vigiliae. Non equidem invideo, miror magis posuere velit aliquet. Quisque placerat facilisis egestas cillum dolore. Curabitur blandit tempus ardua ridiculus sed magna. Contra legem facit qui id facit quod lex prohibet. Petierunt uti sibi concilium totius Galliae in diem certam indicere.</p>
+    </div> -->
     <!-- Logo -->
-    <div class="logo-wrapper">
+    <div id="logo-wrapper" class="logo-wrapper">
         <img class="header-logo" src="assets/all-pages/tls-horizontal-logo-white.svg" alt="The LaSallian Logo">
         
     </div>
@@ -10,11 +14,11 @@
         <img class="cover-art-img" src="assets/homepage/cover/art-transparentbg.png" alt="">
     </div>
     <!-- Text -->
-    <div class="header-text-wrapper">
+    <div id="header-text-wrapper" class="header-text-wrapper">
         <h1 class="header-text-title display-1 mb-3 mb-lg-4">On different pages</h1>
         <h2 class="header-text-subtitle mb-4">2023 General Elections Special</h2>
         <div class="header-buttons-wrapper">
-            <button id="about-cover-btn" class="button button-white">About the Cover</button>
+            <button id="about-cover-btn" class="button button-white" onclick="displayAboutTheCover()">About the Cover</button>
             <a id="view-content-btn" href="#" class="button button-white">View Content</a>
         </div>
     </div>
