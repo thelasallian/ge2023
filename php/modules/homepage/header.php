079120c4f87679f891a02c43e43b448d99801870
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-07-08 15:40:04 +0800

    Added basic structure for header

---
+++ php/modules/homepage/header.php
@@ -0,0 +1,16 @@
+<div class="header-wrapper">
+    <header>
+        <!-- Logo -->
+        <div class="logo-wrapper">
+            
+        </div>
+        <!-- Art -->
+        <div class="cover-art-wrapper">
+
+        </div>
+        <!-- Text -->
+        <div class="header-text-wrapper">
+
+        </div>
+    </header>
+</div>
