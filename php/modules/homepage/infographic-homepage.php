fde5b604d6148589e1ebcc380e382d454e7e6e33
Jeremy Wang <jeremywang512@gmail.com>
2023-07-09 20:37:48 +0800

    infographic implementation

---
+++ php/modules/homepage/infographic-homepage.php
@@ -0,0 +1,9 @@
+<div class="info-bg-color">
+    <div class="container d-flex flex-column align-items-center py-5">
+        <span class="info-tag">INFOGRAPHIC</span>
+        <div class="info-container">
+            <p class="info-text md-0"><strong>Voter turnout</strong> in the morbi fringilla convallis sapien</p>
+        </div>
+        <a type="button" class="view-button button btn btn-outline-light mx-2">VIEW</a>
+    </div>
+</div>
