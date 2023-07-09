673be87be8b68c33a13b786cc01d1eea056cc204
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-07-09 15:51:28 +0800

    Functional desktop grid

--- php/modules/vox-populi/desktop-grid.php
+++ php/modules/vox-populi/desktop-grid.php
@@ -1,5 +1,35 @@
 <section id="vox-populi-desktop-grid">
-    <div class="container">
-         
+    <div class="container-fluid">
+        <div class="row row-cols-3 g-4 g-lg-5">
+            <?php
+            // Read the JSON file
+            $jsonString = file_get_contents('json/vox-populi-all.json');
+
+            // Convert the JSON string to a PHP array
+            $photos = json_decode($jsonString, true);
+
+            // Check if decoding was successful
+            if ($photos === null) {
+                // Handle the error if decoding fails
+                die('Error decoding JSON');
+            }
+
+            foreach ($photos as $photo) :
+            ?>
+
+                <div class="col">
+                    <div class="ds-photo-wrapper">
+                        <div class="ds-photo-quote-wrapper">
+                            <p class="ds-quote"><?php echo $photo["quote"]; ?></p>
+                            <p class="ds-byline"><?php echo $photo["byline"]; ?></p>
+                        </div>
+                        <img src="<?php echo $photo["image-path"]; ?>" alt="" class="ds-photo-img">
+                    </div>
+                </div>
+
+            <?php  
+            endforeach;
+            ?>
+        </div>
     </div>
 </section>
