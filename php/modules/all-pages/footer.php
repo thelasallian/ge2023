06cfbb899f0da92928293cd92903310ff1cb9203
Jeremy Wang <jeremywang512@gmail.com>
2023-07-09 11:50:54 +0800

    Footer contents

---
+++ php/modules/all-pages/footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <div class="footer">
+        <div class="container py-5 flex-column align-items-center mx-auto">
+            <img src="assets\all-pages\tls-vertical-logo-white.svg" alt="TLS Logo">
+            <p class="my-auto">Copyright 2023 © <strong>The LaSallian</strong><br>
+            <i>Website by</i> <a href=""><strong>Ramon Martinez</strong></a>, <a href=""><strong>Ronn Parcia</strong></a>, & <a href="https://github.com/jerw18"><strong>Jeremy Wang</strong></a><br>
+            <i>Cover by</i> <strong>Alexia Roman</strong><br>
+            </p>
+        </div>
+    </div>
+</footer>
