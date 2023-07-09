7394c1956d68acc06fd48bb8510737f7ed22f90a
Jeremy Wang <jeremywang512@gmail.com>
2023-07-09 20:54:54 +0800

    fixed some spacing issues

--- php/modules/homepage/infographic-homepage.php
+++ php/modules/homepage/infographic-homepage.php
@@ -1,6 +1,6 @@
 <div class="info-bg-color">
     <div class="container d-flex flex-column align-items-center py-5">
-        <span class="info-tag">INFOGRAPHIC</span>
+        <span class="info-tag pb-2">INFOGRAPHIC</span>
         <div class="info-container">
             <p class="info-text md-0"><strong>Voter turnout</strong> in the morbi fringilla convallis sapien</p>
         </div>
