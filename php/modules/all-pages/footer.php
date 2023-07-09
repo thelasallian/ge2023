eaefc61a134750588a9847961a23d5d02418bd9e
Jeremy Wang <jeremywang512@gmail.com>
2023-07-09 14:12:40 +0800

    finished footer layout

--- php/modules/all-pages/footer.php
+++ php/modules/all-pages/footer.php
@@ -1,14 +1,16 @@
 <footer>
     <div class="footer">
         <div class="container py-5 d-flex flex-column align-items-center mx-auto">
-            <img src="assets\all-pages\tls-vertical-logo-white.svg" alt="TLS Logo">
+            <div class="logo">
+                <img src="assets\all-pages\tls-vertical-logo-white.svg" alt="TLS Logo">
+            </div>
             <p class="text py-1 fs-4">On different pages</p>
-            <div class="py-3 row align-items-center">
-                <button type="button" class="custom-button btn btn-outline-light col mx-3">VISIT MAIN WEBSITE</button>
-                <button type="button" class="custom-button btn btn-outline-light col mx-3">VIEW MAGAZINE</button>
+            <div class="py-3 row flex-wrap">
+                <a type="button" class="custom-button button btn btn-outline-light mx-3 col">VISIT MAIN WEBSITE</a>
+                <a type="button" class="custom-button button btn btn-outline-light mx-3 col">VIEW MAGAZINE</a>
             </div>
             <p class="credits pt-3">Copyright 2023 © <strong>The LaSallian</strong><br>
-            <i>Website by</i> <a href=""><strong>Ramon Enrico Martinez</strong></a>, <a href=""><strong>Ronn Parcia</strong></a>, & <a href="https://github.com/jerw18"><strong>Jeremy Wang</strong></a><br>
+            <i>Website by</i> <a class="text" href=""><strong>Ramon Enrico Martinez</strong></a>, <a class="text" href=""><strong>Ronn Parcia</strong></a>, & <a class="text" href="https://github.com/jerw18"><strong>Jeremy Wang</strong></a><br>
             <i>Cover by</i> <strong>Alexia Roman</strong><br>
             </p>
         </div>
