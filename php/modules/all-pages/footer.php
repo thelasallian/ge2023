e19b45ccf694c49390adf13dfdef24231fb6fb9c
Jeremy Wang <jeremywang512@gmail.com>
2023-07-09 17:41:15 +0800

    blended rectangle behind logo

--- php/modules/all-pages/footer.php
+++ php/modules/all-pages/footer.php
@@ -1,13 +1,13 @@
 <footer>
     <div class="footer">
         <div class="container py-5 d-flex flex-column align-items-center mx-auto">
-            <div class="logo">
+            <div class="logo pt-2">
                 <img src="assets\all-pages\tls-vertical-logo-white.svg" alt="TLS Logo">
             </div>
-            <p class="text py-1 fs-4">On different pages</p>
+            <p class="text py-2 fs-4">On different pages</p>
             <div class="py-3 row flex-wrap">
-                <a type="button" class="custom-button button btn btn-outline-light mx-3 col">VISIT MAIN WEBSITE</a>
-                <a type="button" class="custom-button button btn btn-outline-light mx-3 col">VIEW MAGAZINE</a>
+                <a type="button" class="custom-button button btn btn-outline-light mx-2 col">VISIT MAIN WEBSITE</a>
+                <a type="button" class="custom-button button btn btn-outline-light mx-2 col">VIEW MAGAZINE</a>
             </div>
             <p class="credits pt-3">Copyright 2023 © <strong>The LaSallian</strong><br>
             <i>Website by</i> <a class="text" href=""><strong>Ramon Enrico Martinez</strong></a>, <a class="text" href=""><strong>Ronn Parcia</strong></a>, & <a class="text" href="https://github.com/jerw18"><strong>Jeremy Wang</strong></a><br>
