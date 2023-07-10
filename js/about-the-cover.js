d1c43d624aae5d1e2f98ad91e52969e5697153e1
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-07-10 11:45:53 +0800

    Working about the cover

--- js/about-the-cover.js
+++ js/about-the-cover.js
@@ -1,23 +1,14 @@
 function displayAboutTheCover() {
-    var logo = document.getElementById("logo-wrapper");
-    var text = document.getElementById("header-text-wrapper");
     var aboutTheCover = document.getElementById("about-the-cover");
-    var art = document.getElementById("cover-art-img");
 
-    logo.style.display = "none";
-    text.style.display = "none";
-    aboutTheCover.style.display = "flex";
-    art.classList.add("py-0");
+    
+    aboutTheCover.style.opacity = "1";
+    aboutTheCover.style.pointerEvents = "all";
 }
 
 function hideAboutTheCover() {
-    var logo = document.getElementById("logo-wrapper");
-    var text = document.getElementById("header-text-wrapper");
     var aboutTheCover = document.getElementById("about-the-cover");
-    var art = document.getElementById("cover-art-img");
 
-    logo.style.display = "block";
-    text.style.display = "block";
-    aboutTheCover.style.display = "none";
-    art.classList.remove("py-0");
+    aboutTheCover.style.opacity = "0";
+    aboutTheCover.style.pointerEvents = "none";
 }
