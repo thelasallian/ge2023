0ed2b520a62eafb5410481f11e84fe36e9075c07
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-08-22 20:05:59 +0800

    Elegant + efficient responsive articles using CSS grid

--- php/modules/homepage/articles.php
+++ php/modules/homepage/articles.php
@@ -1,6 +1,30 @@
+<?php 
+// Read list of articles
+$jsonString = file_get_contents('json/articles.json'); // Read the articles JSON file
+$articles = json_decode($jsonString, true); // Convert the JSON string to a PHP array
+
+// Check if decoding was successful
+if ($articles === null) {
+    die('Error decoding JSON'); // Handle the error if decoding fails
+}
+
+$count = 1;
+?>
+
 <section id="articles">
     <div class="container">
-        
+        <?php if ($articles !== null): // If articles were decoded successfully, display grid ?>
+            <!-- Grid -->
+            <div class="articles-grid">
+                <?php foreach ($articles as $a): // Create grid item per article, use count for class name ?>
+                    <!-- Article <?= $count; ?> -->
+                    <div class="article article-<?= $count; ?>">
+                        Article <?= $count; ?>
+                    </div>
 
+                    <?php $count++; // For article-$ class ?>
+                <?php endforeach; ?>
+            </div>
+        <?php endif; ?>
     </div>
 </section>
