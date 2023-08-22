a8601ac013040210a30d347ce765d792ce5b55e3
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-08-22 22:53:39 +0800

    Header for Desktop

---
+++ php/modules/homepage/header.php
@@ -0,0 +1,35 @@
+<div class="header-wrapper">
+    <!-- Header -->
+    <header>
+        <!-- Title and Buttons Wrapper -->
+        <div class="title-btns-wrapper">
+            <!-- Title -->
+            <h1 class="title">On different pages</h1>
+
+            <!-- Buttons -->
+            <div class="btns-wrapper">
+                <!-- About the Cover Button -->
+                <button 
+                    id="about-cover-btn" 
+                    class="button button-white" 
+                    onclick="displayAboutTheCover()"
+                >
+                    About the Cover
+                </button>
+                <!-- View Content Button -->
+                <a 
+                    id="view-content-btn" 
+                    href="#articles" 
+                    class="button button-white"
+                >
+                    View Content
+                </a>
+            </div>
+        </div>
+
+        <!-- Art Wrapper -->
+        <div class="art-wrapper">
+            <div class="art-landscape"></div>
+        </div>
+    </header>
+</div>
