647aabfb122b0c2bde9a07813f423cbcf8986593
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-08-23 09:22:23 +0800

    Behavior for landscape art

--- php/modules/homepage/header.php
+++ php/modules/homepage/header.php
@@ -41,6 +41,33 @@
 
     <!-- About the Cover -->
     <section id="about-the-cover">
-        Hello world
+        <!-- Details -->
+        <div class="details">
+            <!-- Title -->
+            <h2>About the Cover</h2>
+            <p>
+                The cover explores the evolution of an idea into the 
+                University Student Government we know today. It should 
+                aim to move forward but not forget; it should strive to 
+                stay grounded but never limit itself. We encourage our 
+                readers—no matter their personal opinions—to reflect on 
+                the origins of the USG, as well as the advancing standards 
+                it must meet to keep up with the times.
+            </p>
+            <!-- Back Button -->
+            <button 
+                    id="about-cover-btn" 
+                    class="button button-white" 
+                    onclick="hideAboutTheCover()"
+            >
+                Back
+            </button>
+        </div>
+        <!-- Art -->
+        <div class="atc-art-wrapper">
+            <div class="art-landscape"></div>
+            <!-- Art for Portrait Screens -->
+            <img src="assets/homepage/cover/cover-art-portrait.png" alt="" class="art-portrait">
+        </div>
     </section>
 </div>
