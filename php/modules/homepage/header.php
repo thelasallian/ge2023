1e31cde857d57bc7f4fe064db9271b385be4a3ae
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-08-23 12:53:06 +0800

    Code formatting

--- php/modules/homepage/header.php
+++ php/modules/homepage/header.php
@@ -4,7 +4,9 @@
         <!-- Title and Buttons Wrapper -->
         <div class="title-btns-wrapper">
             <!-- TLS Star -->
-            <img src="assets/all-pages/tls-star-white.svg" alt="" class="tls-star">
+            <img 
+              src="assets/all-pages/tls-star-white.svg" 
+              class="tls-star">
 
             <!-- Title -->
             <h1 class="title">
@@ -16,18 +18,15 @@
             <div class="btns-wrapper">
                 <!-- About the Cover Button -->
                 <button 
-                    id="about-cover-btn" 
-                    class="button button-white" 
-                    onclick="displayAboutTheCover()"
-                >
+                  id="about-cover-btn" 
+                  class="button button-white" 
+                  onclick="displayAboutTheCover()">
                     About the Cover
                 </button>
                 <!-- View Content Button -->
-                <a 
-                    id="view-content-btn" 
-                    href="#navbar" 
-                    class="button button-white"
-                >
+                <a id="view-content-btn" 
+                  href="#navbar" 
+                  class="button button-white">
                     View Content
                 </a>
             </div>
@@ -38,7 +37,9 @@
             <!-- Art for Landscape Screens (Faux Image Hack) -->
             <div class="art-landscape"></div>
             <!-- Art for Portrait Screens -->
-            <img src="assets/homepage/cover/cover-art-portrait.png" alt="" class="art-portrait">
+            <img 
+              src="assets/homepage/cover/cover-art-portrait.png" 
+              class="art-portrait" />
         </div>
     </header>
 
@@ -49,28 +50,29 @@
             <!-- Title -->
             <h2>About the Cover</h2>
             <p>
-                The cover explores the evolution of an idea into the 
-                University Student Government we know today. It should 
-                aim to move forward but not forget; it should strive to 
-                stay grounded but never limit itself. We encourage our 
-                readers—no matter their personal opinions—to reflect on 
-                the origins of the USG, as well as the advancing standards 
-                it must meet to keep up with the times.
+                The cover explores the evolution of an idea into the University 
+                Student Government we know today. It should aim to move forward 
+                but not forget; it should strive to stay grounded but never limit 
+                itself. We encourage our readers—no matter their personal 
+                opinions—to reflect on the origins of the USG, as well as the 
+                advancing standards it must meet to keep up with the times.
             </p>
             <!-- Back Button -->
             <button 
-                    id="about-cover-btn" 
-                    class="button button-white" 
-                    onclick="hideAboutTheCover()"
-            >
+              id="about-cover-btn" 
+              class="button button-white" 
+              onclick="hideAboutTheCover()">
                 Back
             </button>
         </div>
         <!-- Art -->
         <div class="atc-art-wrapper">
+            <!-- Art for Landscape Screens -->
             <div class="art-landscape"></div>
             <!-- Art for Portrait Screens -->
-            <img src="assets/homepage/cover/cover-art-portrait.png" alt="" class="art-portrait">
+            <img 
+              src="assets/homepage/cover/cover-art-portrait.png" 
+              class="art-portrait">
         </div>
     </section>
 </div>
