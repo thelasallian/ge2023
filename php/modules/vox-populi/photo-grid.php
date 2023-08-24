c0d5d0a4dac9ec15c78fc571765effd0299a58c8
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-08-24 14:16:35 +0800

    Responsive grid

--- php/modules/vox-populi/photo-grid.php
+++ php/modules/vox-populi/photo-grid.php
@@ -1,3 +1,28 @@
+<?php 
+// Load the photos
+$json = file_get_contents("json/vox-populi.json"); // Read JSON file
+$photos = json_decode($json, true); // Decode JSON to an associative array
+
+// Check if decoding was successful
+if ($photos === null) {
+    die("Error decoding JSON");
+}
+?>
+
 <section id="vox-populi">
-    
+    <?php if ($photos !== null): // If photos were loaded successfully ?>
+        <div class="grid-container">
+            <?php foreach ($photos as $p): // For each photo, make a grid item ?>
+                <div class="grid-item <?= $p["stand"]; ?>">
+                    <img 
+                        src="<?= $p["image-path"]; ?>"
+                        class="photo <?= $p["stand"]; ?>">
+                    <div class="details">
+                        <p class="quote"><?= $p["quote"]; ?></p>
+                        <p class="byline"><?= $p["byline"]; ?></p>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    <?php endif; ?>
 </section>
