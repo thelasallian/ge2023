f6736f92284f285a94f64e2dcdb785bbe2fc8d57
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-08-24 17:19:06 +0800

    Code formatting + TODOs

--- php/modules/vox-populi/top-portion.php
+++ php/modules/vox-populi/top-portion.php
@@ -1,16 +1,30 @@
 <header id="vox-populi-header">
     <div class="container d-flex flex-column align-items-center">
         <a id="back-button" href="index.php" class="button button-white align-self-start">← Back</a>
-        <h1 class="title">Vox Populi</h1>
-        <h2 class="subtitle"><span id="typed"></span></h2>
-        <p class="photographer-byline">by <span class="photographer">Michael Hamza Mustapha</span> & <span class="photographer">Gilliane Luy</span></p>
+        <h1 class="title">
+            Vox Populi
+        </h1>
+        <h2 class="subtitle">
+            <span id="typed"></span>
+        </h2>
+        <p class="photographer-byline">
+            <!-- TODO: Verify if these are the only photographer -->
+            by <span class="photographer">Michael Hamza Mustapha</span> & 
+            <span class="photographer">Gilliane Luy</span>
+        </p>
         <p class="description">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam velit animi, quia nisi voluptates laboriosam minus, sint ducimus id natus neque maiores aliquid perspiciatis hic asperiores dignissimos assumenda. Dolore quo explicabo eum ullam, cum minus iste facere perferendis rerum minima, tenetur error magnam, iure odit eligendi dolores aliquam accusantium corporis.
+            <!-- TODO: Replace with actual description -->
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam 
+            velit animi, quia nisi voluptates laboriosam minus, sint ducimus 
+            id natus neque maiores aliquid perspiciatis hic asperiores dignissimos 
+            assumenda. Dolore quo explicabo eum ullam, cum minus iste facere 
+            perferendis rerum minima, tenetur error magnam, iure odit eligendi 
+            dolores aliquam accusantium corporis.
         </p>
     </div>
 </header>
 
 <div class="container d-flex justify-content-center">
-    <!-- TODO: Don't forget to change -->
-    <h3 class="vox-populi-question">Kaya pa today, sis?</h3>
+    <!-- TODO: Replace with actual question -->
+    <h3 class="vox-populi-question">Lorem ipsum dolor sit amet consectetur?</h3>
 </div>
