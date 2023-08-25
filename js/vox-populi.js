e26b3db73308e837fd9acec908f9fcd8865d0de7
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-08-25 14:06:17 +0800

    Added more comments and used descriptive variable names

--- js/vox-populi.js
+++ js/vox-populi.js
@@ -9,6 +9,12 @@ const buttons = document.querySelectorAll(".vox-btn");
 function setPhotosDisplay() {
     if (mediaQuery.matches) { //If the screen width is >= 768px
         // Display all photos
+        // Note: This could have been implemented using a CSS media query, but
+        // there needs to be a way to reset the display property of the photos
+        // if the screen went from mobile to desktop AND the user had previously
+        // filtered the photos displayed based on stand, which uses JavaScript
+        // to set the display property. That's why this function is called
+        // inside an event listener for changes in screen width.
         photos.forEach(photo => {
             photo.style.display = "block";
         });
@@ -42,18 +48,19 @@ setPhotosDisplay(); // Initial call
 // For each Vox Populi button, add an event listener that will filter the grid items
 // and set the active class if clicked.
 buttons.forEach(button => {
+    // Add event listener to button
     button.addEventListener("click", () => {
-        const className = button.id; // Either "yes", "no", or "undecided"
+        const stand = button.id; // Either "yes", "no", or "undecided"
 
         // For each photo, if it matches the stand (yes/no/undecided)
         // of the button clicked, display it. Otherwise, hide it.
         photos.forEach(photo => {
-            photo.style.display = photo.classList.contains(className) ? "block" : "none";
+            photo.style.display = photo.classList.contains(stand) ? "block" : "none";
         });
 
         // For each button, if it is the button clicked, add the "active" class.
         buttons.forEach(button => {
-            if (button.id === className) {
+            if (button.id === stand) {
                 button.classList.add("active");
             } else {
                 button.classList.remove("active");
