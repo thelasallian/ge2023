2b71c0b3c7b07d4186acc343f8b14c0241fda9e3
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-08-29 14:16:39 +0800

    Removed unnecessary divs and styles

--- php/all-pages/footer.php
+++ php/all-pages/footer.php
@@ -1,21 +1,19 @@
-<footer class="pt-5">
-    <div class="footer">
-        <div class="container py-5 d-flex flex-column align-items-center mx-auto">
-            <div class="logo pt-2">
-                <img src="assets\all-pages\tls-vertical-logo-white.svg" alt="TLS Logo">
-            </div>
-            <p class="text py-2 fs-4">On different pages</p>
-            <div class="footer-button-wrapper py-3">
-                <a href="https://thelasallian.com" class="button button-white">Visit Main Website</a>
-                <a href="#" class="button button-white">View Magazine</a>
-            </div>
-            <p class="credits pt-3">Copyright 2023 © <strong>The LaSallian</strong><br>
-                <i>Website by</i> 
-                <strong>Ramon Enrico Martinez</strong>,
-                <a class="text" href="https://github.com/ronnparcia"><strong>Ronn Parcia</strong></a>, & 
-                <a class="text" href="https://github.com/jerw18"><strong>Jeremy Wang</strong></a><br>
-                <i>Cover by</i> <strong>Alexia Roman</strong><br>
-            </p>
+<footer>
+    <div class="container py-5 d-flex flex-column align-items-center">
+        <div class="logo">
+            <img src="assets\all-pages\tls-vertical-logo-white.svg" alt="TLS Logo">
         </div>
+        <p class="text py-2 fs-4">On different pages</p>
+        <div class="footer-button-wrapper py-3">
+            <a href="https://thelasallian.com" class="button button-white">Visit Main Website</a>
+            <a href="#" class="button button-white">View Magazine</a>
+        </div>
+        <p class="credits pt-3">Copyright 2023 © <strong>The LaSallian</strong><br>
+            <i>Website by</i> 
+            <strong>Ramon Enrico Martinez</strong>,
+            <a class="text" href="https://github.com/ronnparcia"><strong>Ronn Parcia</strong></a>, & 
+            <a class="text" href="https://github.com/jerw18"><strong>Jeremy Wang</strong></a><br>
+            <i>Cover by</i> <strong>Alexia Roman</strong><br>
+        </p>
     </div>
 </footer>
