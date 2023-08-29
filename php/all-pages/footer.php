bfcb10e303c86f5f5782dd8c8766b93d5df23f92
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-08-29 14:25:52 +0800

    Descriptive names + bracket spaces

--- php/all-pages/footer.php
+++ php/all-pages/footer.php
@@ -3,7 +3,7 @@
         <div class="logo">
             <img src="assets\all-pages\tls-vertical-logo-white.svg" alt="TLS Logo">
         </div>
-        <p class="text py-2 fs-4">On different pages</p>
+        <p class="title py-2 fs-4">On different pages</p>
         <div class="btns-wrapper py-3">
             <a href="https://thelasallian.com" class="button button-white">Visit Main Website</a>
             <a href="#" class="button button-white">View Magazine</a>
@@ -11,8 +11,8 @@
         <p class="credits pt-3">Copyright 2023 © <strong>The LaSallian</strong><br>
             <i>Website by</i> 
             <strong>Ramon Enrico Martinez</strong>,
-            <a class="text" href="https://github.com/ronnparcia"><strong>Ronn Parcia</strong></a>, & 
-            <a class="text" href="https://github.com/jerw18"><strong>Jeremy Wang</strong></a><br>
+            <a class="link" href="https://github.com/ronnparcia"><strong>Ronn Parcia</strong></a>, & 
+            <a class="link" href="https://github.com/jerw18"><strong>Jeremy Wang</strong></a><br>
             <i>Cover by</i> <strong>Alexia Roman</strong><br>
         </p>
     </div>
