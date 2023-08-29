9586da762bb05b956b6031d4dc3d571474c97cb9
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-08-29 08:29:54 +0800

    Max width on infog title + actual title

--- php/homepage/infographic.php
+++ php/homepage/infographic.php
@@ -1,7 +1,7 @@
 <section id="infographic" class="band">
     <div class="container">
         <span data-aos="zoom-in" class="tag tag-white">Infographic</span>
-        <h2 class="title"><strong>Voter turnout</strong> in the morbi fringilla convallis sapien</h2>
+        <h2 class="title">Voter turnout<br/> through the years</h2>
         <a data-aos="zoom-in" class="button button-white button-white-over-green">View</a>
     </div>
 </section>
