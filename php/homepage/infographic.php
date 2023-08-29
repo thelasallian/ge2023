078f3e194f30de2340132d902ca590c6bcddf28f
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-08-29 08:28:28 +0800

    Removed unnecessary elements

--- php/homepage/infographic.php
+++ php/homepage/infographic.php
@@ -1,9 +1,7 @@
 <section id="infographic" class="band">
     <div class="container">
         <span data-aos="zoom-in" class="tag tag-white">Infographic</span>
-        <div data-aos="zoom-in" class="info-container">
-            <h2 class="title"><strong>Voter turnout</strong> in the morbi fringilla convallis sapien</h2>
-        </div>
-        <a data-aos="zoom-in"  type="button" class="button button-white button-white-over-green">View</a>
+        <h2 class="title"><strong>Voter turnout</strong> in the morbi fringilla convallis sapien</h2>
+        <a data-aos="zoom-in" class="button button-white button-white-over-green">View</a>
     </div>
 </section>
