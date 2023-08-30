95951865082fa5f886a33c295d031c057a11dd31
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-08-30 15:04:09 +0800

    Trigger navbar visibility scroll past header

--- js/homepage/navbar.js
+++ js/homepage/navbar.js
@@ -1,6 +1,23 @@
 // When clicking the menu button, toggle the visibility of the links container
+const linksContainer = document.getElementById("links-container");
+
 function showLinks() {
-    const linksContainer = document.getElementById("links-container");
-    
     linksContainer.classList.toggle("active");
 }
+
+// Show only the navbar when beyond the header
+const header = document.querySelector('header');
+const nav = document.querySelector('nav');
+
+const headerHeight = header.offsetHeight;
+
+window.addEventListener('scroll', () => {
+    const scrollPos = window.scrollY;
+
+    if (scrollPos > headerHeight) {
+        nav.classList.add("active");
+    } else {
+        nav.classList.remove("active");
+        linksContainer.classList.remove("active");
+    }
+});
