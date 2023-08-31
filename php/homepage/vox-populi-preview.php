77b81e75ebe25ddbf17aff97d8427e952e24054f
Jeremy Wang <jeremywang512@gmail.com>
2023-08-31 16:07:31 +0800

    vox populi implementation

---
+++ php/homepage/vox-populi-preview.php
@@ -0,0 +1,19 @@
+<section id="vox-populi">
+    <div class="container">
+        <span class="tag tag-green">VOX POPULI</span>
+        <h2 class="title"> Thoughts on a two-party system</h2>
+        <a href="vox-populi.php" class="button button-black">View Gallery</a>
+
+        <div class="grid-container">
+            <div class="grid-item">
+                <img src="assets/vox-populi/02-vox.jpg" class="photo">
+            </div>
+            <div class="grid-item">
+                <img src="assets/vox-populi/02-vox.jpg" class="photo">
+            </div>
+            <div class="grid-item">
+                <img src="assets/vox-populi/02-vox.jpg" class="photo">
+            </div>
+        </div>
+    </div>
+</section>
