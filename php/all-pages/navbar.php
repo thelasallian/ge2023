69009c8288390b420dd3004fde5a5a8f8ab5654c
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-09-02 06:56:41 +0800

    Moved the actual nav drawer out of the sticky navbar

--- php/all-pages/navbar.php
+++ php/all-pages/navbar.php
@@ -2,7 +2,7 @@
 
 ?>
 
-<nav class="sticky-top">
+<div id="nav-sticky" class="sticky-top">
     <div class="container">
         <!-- Logo -->
         <a href="/">
@@ -12,37 +12,38 @@
         <button class="nav-btn" onclick="showLinks()">
             <span class="material-icons">menu</span>
         </button>
-        <!-- Nav Links -->
-        <div id="nav-drawer" class="nav-drawer">
-            <!-- Close Button -->
-            <button class="nav-btn" id="close-btn" onclick="closeLinks()">
-                <span class="material-icons">close</span>
-            </button>
-            <!-- TLS Logo (White) -->
-            <a href="/">
-                <img src="assets/all-pages/tls-horizontal-logo-white.svg" id="white-logo">
-            </a>
-            <!-- Anchor Links -->
-            <ul>
-                <li>
-                    <a href="#articles" class="nav-anchor">
-                        <img src="assets/homepage/articles/03-women.jpg" class="link-img">
-                        Articles
-                    </a>
-                </li>
-                <li>
-                    <a href="#infographic" class="nav-anchor">
-                        <img src="assets/homepage/articles/04-voterturnout.jpg" class="link-img">
-                        Voter turnout through the years
-                    </a>
-                </li>
-                <li>
-                    <a href="#vox-populi" class="nav-anchor">
-                        <img src="assets/homepage/articles/05-usg.jpg" class="link-img">
-                        Thoughts on a two-party system
-                    </a>
-                </li>
-            </ul>
-        </div>
     </div>
+</div>
+
+<!-- Nav Links -->
+<nav id="nav-drawer" class="nav-drawer">
+    <!-- Close Button -->
+    <button class="nav-btn" id="close-btn" onclick="closeLinks()">
+        <span class="material-icons">close</span>
+    </button>
+    <!-- TLS Logo (White) -->
+    <a href="/">
+        <img src="assets/all-pages/tls-horizontal-logo-white.svg" id="white-logo">
+    </a>
+    <!-- Anchor Links -->
+    <ul>
+        <li>
+            <a href="#articles" class="nav-anchor">
+                <img src="assets/homepage/articles/03-women.jpg" class="link-img">
+                Articles
+            </a>
+        </li>
+        <li>
+            <a href="#infographic" class="nav-anchor">
+                <img src="assets/homepage/articles/04-voterturnout.jpg" class="link-img">
+                Voter turnout through the years
+            </a>
+        </li>
+        <li>
+            <a href="#vox-populi" class="nav-anchor">
+                <img src="assets/homepage/articles/05-usg.jpg" class="link-img">
+                Thoughts on a two-party system
+            </a>
+        </li>
+    </ul>
 </nav>
