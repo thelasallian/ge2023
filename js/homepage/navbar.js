c20956dd48131faaae97a82b918289576695fd4c
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-09-02 07:04:14 +0800

    Removed hiding of navbar on header past

--- js/homepage/navbar.js
+++ js/homepage/navbar.js
@@ -7,24 +7,6 @@ function showNavDrawer() {
     greenLogo.classList.toggle("hidden");
 }
 
-// Show only the navbar when beyond the header
-const header = document.querySelector('header');
-const nav = document.querySelector('nav');
-
-const headerHeight = header.offsetHeight;
-
-window.addEventListener('scroll', () => {
-    const scrollPos = window.scrollY;
-
-    if (scrollPos > headerHeight - 50) {
-        nav.classList.add("active");
-        greenLogo.classList.remove("hidden");
-    } else {
-        nav.classList.remove("active");
-        navDrawer.classList.remove("active");
-    }
-});
-
 // Close the navbar on click
 function closeNavDrawer() {
     navDrawer.classList.remove("active");
