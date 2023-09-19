f1b460fb17ca3061c1ac7cc198757a288c14b3d8
Jeremy Wang <jeremywang512@gmail.com>
2023-09-19 20:47:29 +0800

    added animation

--- php/homepage/vox-populi-preview.php
+++ php/homepage/vox-populi-preview.php
@@ -1,19 +1,13 @@
 <section id="vox-populi">
-    <div class="container">
+    <div class="container" data-aos="zoom-in">
         <span class="tag tag-green">VOX POPULI</span>
         <h2 class="title"> Thoughts on a two-party system</h2>
         <a href="vox-populi.php" class="button button-black">View Gallery</a>
 
-        <div class="grid-container">
-            <img class="grid-item" src="assets/vox-populi/02-vox.jpg">
-            <img class="grid-item" src="assets/vox-populi/02-vox.jpg">
-            <img class="grid-item" src="assets/vox-populi/02-vox.jpg">
-            <!-- <div class="grid-item" style="background-image: url(assets/vox-populi/02-vox.jpg)">
-            </div>
-            <div class="grid-item" style="background-image: url(assets/vox-populi/02-vox.jpg)">
-            </div>
-            <div class="grid-item" style="background-image: url(assets/vox-populi/02-vox.jpg)">
-            </div> -->
+        <div class="grid-container" >
+            <img class="grid-item" src="assets/vox-populi/01-vox.jpg" data-aos="zoom-in-up" data-aos-duration="1000">
+            <img class="grid-item" src="assets/vox-populi/02-vox.jpg" data-aos="zoom-in-up" data-aos-duration="1500">
+            <img class="grid-item" src="assets/vox-populi/03-vox.jpg" data-aos="zoom-in-up" data-aos-duration="2000">
         </div>
     </div>
 </section>
