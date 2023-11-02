090a68c3ee6be95415737d1232f150c68431a804
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-11-02 22:47:49 +0800

    Added CP H2Hs

--- php/head-to-head/grid.php
+++ php/head-to-head/grid.php
@@ -1,7 +1,7 @@
 <?php
 // Convert JSON to associative array (true parameter)
 $h2h_eb = json_decode(file_get_contents('https://raw.githubusercontent.com/thelasallian/ge2023/main/json/h2h-eb.json'), true);
-$hsh_cp = json_decode(file_get_contents('https://raw.githubusercontent.com/thelasallian/ge2023/main/json/h2h-cp.json'), true);
+$h2h_cp = json_decode(file_get_contents('https://raw.githubusercontent.com/thelasallian/ge2023/main/json/h2h-cp.json'), true);
 ?>
 
 <div class="container">
@@ -18,6 +18,26 @@ $hsh_cp = json_decode(file_get_contents('https://raw.githubusercontent.com/thela
                         </span>
                         <h2 class="h2hArticle-title">
                             <?= $eb["title"]; ?>
+                        </h2>
+                    </div>
+                </a>
+            </div>
+        <?php endforeach; ?>
+    </div>
+
+    <div class="h2hGrid" id="h2h-CP">
+        <?php foreach ($h2h_cp as $cp) : ?>
+            <div class="h2hArticle">
+                <a href="<?= $cp["link"]; ?>">
+                    <div class="h2hArticle-imgWrapper">
+                        <img src="<?= $cp["visual"]; ?>" alt="" class="h2hArticle-img">
+                    </div>
+                    <div class="h2hArticle-Info">
+                        <span class="tag tag-green">
+                            <?= $cp["position"]; ?>
+                        </span>
+                        <h2 class="h2hArticle-title">
+                            <?= $cp["title"]; ?>
                         </h2>
                     </div>
                 </a>
