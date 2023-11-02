e17f12170f3554ce0eb6a61219ab137faf0800f0
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-11-02 17:00:58 +0800

    Added article and charts

---
+++ php/voter-turnout/article.php
@@ -0,0 +1,56 @@
+<div class="article-container">
+    <p>
+        Previous University Student Government elections had seen- significant increases in voter
+        population for both Manila and Laguna campuses, yet voter turnouts remain around or barely
+        above the midpoint. In the past five years, the highest election turnout was 60.02 percent during
+        the 2021 Make-Up Elections, while the most recent 2022 Make-Up Elections was barely
+        validated despiteemie two-day extension at 51.83 percent.
+    </p>
+    <p>
+        The problem of the lack of voting participation from the student body has been a longstanding
+        problem for the USG. Back during the 2015 General Elections, DLSU Comelec was forced to
+        hold the start of of office by the second term of the academic year due to a plebiscite failure after
+        receiving a voter turnout of 35.90 percent, well below the 50 percent plus one requirement.
+    </p>
+    <!-- Average turnouts in the last five elections -->
+    <div class="chart-wrapper">
+        <div style="min-height:440px">
+            <script type="text/javascript" defer src="https://datawrapper.dwcdn.net/G5ZTK/embed.js?v=1" charset="utf-8"></script><noscript><img src="https://datawrapper.dwcdn.net/G5ZTK/full.png" alt="" /></noscript>
+        </div>
+    </div>
+    <!-- Three-year Average Turnouts of Uppermost Batches Per College -->
+    <div class="chart-wrapper">
+        <div style="min-height:436px">
+            <script type="text/javascript" defer src="https://datawrapper.dwcdn.net/wpj7D/embed.js?v=1" charset="utf-8"></script><noscript><img src="https://datawrapper.dwcdn.net/wpj7D/full.png" alt="" /></noscript>
+        </div>
+    </div>
+    <p>
+        The problem of only having a passable turnout is observed throughout most colleges and in the
+        Laguna campus, with the exceptions being the School of Economics (SOE) and the Br. Andrew
+        Gonzalez College of Education (BAGCED), whose voter turnout averages areis aboveat around
+        70 percent.
+    </p>
+    <!-- Turnouts in the last five elections -->
+    <div class="chart-wrapper">
+        <div style="min-height:430px">
+            <script type="text/javascript" defer src="https://datawrapper.dwcdn.net/Ere5d/embed.js?v=1" charset="utf-8"></script><noscript><img src="https://datawrapper.dwcdn.net/Ere5d/full.png" alt="" /></noscript>
+        </div>
+    </div>
+    <p>
+        The problem of only having a passable turnout is observed throughout most colleges and in the
+        Laguna campus, with the exceptions being the School of Economics (SOE) and the Br. Andrew
+        Gonzalez College of Education (BAGCED), whose voter turnout averages areis aboveat around
+        70 percent.
+    </p>
+    <!-- Lasallian participation in the latest five elections -->
+    <div class="chart-wrapper">
+        <div style="min-height:620px">
+            <script type="text/javascript" defer src="https://datawrapper.dwcdn.net/KsK2b/embed.js?v=1" charset="utf-8"></script><noscript><img src="https://datawrapper.dwcdn.net/KsK2b/full.png" alt="" /></noscript>
+        </div>
+    </div>
+    <p>
+        With barely any improvements, reversing Lasallians' indifference toward USG politics remains
+        big hurdle thatfer the next administration must work hard to addressie figure out ways to-
+        ifference toward USC polities.
+    </p>
+</div>
