2788176c5e6842d36de965b60669c3f7d0e51f04
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-11-02 13:57:32 +0800

    Updated pics and title with new pics and data

--- php/homepage/vox-populi-preview.php
+++ php/homepage/vox-populi-preview.php
@@ -1,13 +1,13 @@
 <section id="vox-populi">
     <div class="container" data-aos="zoom-in">
         <span class="tag tag-green">VOX POPULI</span>
-        <h2 class="title"> Thoughts on a two-party system</h2>
+        <h2 class="title">What projects or initiatives do you expect from the incoming USG Admin?</h2>
         <a href="vox-populi.php" class="button button-black">View Gallery</a>
 
         <div class="grid-container" >
-            <img class="grid-item" src="assets/vox-populi/01-vox.jpg" data-aos="zoom-in-up" data-aos-duration="1000">
-            <img class="grid-item" src="assets/vox-populi/02-vox.jpg" data-aos="zoom-in-up" data-aos-duration="1500">
-            <img class="grid-item" src="assets/vox-populi/03-vox.jpg" data-aos="zoom-in-up" data-aos-duration="2000">
+            <img class="grid-item" src="assets/vox-populi/bongaros.jpg" data-aos="zoom-in-up" data-aos-duration="1000">
+            <img class="grid-item" src="assets/vox-populi/cayabyab.jpg" data-aos="zoom-in-up" data-aos-duration="1500">
+            <img class="grid-item" src="assets/vox-populi/cruz.jpg" data-aos="zoom-in-up" data-aos-duration="2000">
         </div>
     </div>
 </section>
