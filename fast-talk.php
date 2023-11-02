d7e9ce794156746d7566dec87a3dde7f6981531e
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-11-02 17:18:46 +0800

    Added basic starting files

---
+++ fast-talk.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Meta Tags, Favicons, Google Fonts, Bootstrap, all-pages.css etc. -->
+    <?php require_once 'php/all-pages/head.php'; ?>
+    <link rel="stylesheet" href="css/subpages.css">
+    <link rel="stylesheet" href="css/fast-talk.css">
+
+    <!-- Document Title -->
+    <title>Vox Populi</title>
+</head>
+
+<body>
+    <!-- Header -->
+    <?php include_once 'php/fast-talk/header.php'; ?>
+
+
+    <!-- Footer -->
+    <?php include_once 'php/all-pages/footer.php'; ?>
+
+    <!-- Scripts -->
+    <?php require_once 'php/all-pages/scripts.php'; ?>
+    <script src="js/fast-talk/typed-heading.js"></script>
+</body>
+
+</html>
