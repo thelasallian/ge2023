9784d1ee21f7b0371d33a662fd58bc4403d449a6
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-11-05 00:35:26 +0800

    How to vote preview

---
+++ php/homepage/how-to-vote.php
@@ -0,0 +1,16 @@
+<section id="how-to-vote">
+    <div class="container">
+        <div id="howToVote-wrapper">
+            <div id="howToVote-container">
+                <img src="assets/homepage/how-to-vote.png" class="howToVote-img">
+                <div id="howToVote-info">
+                    <h2 class="title">How to Vote</h2>
+                    <p class="desc">
+                        Fictum, deserunt mollit anim laborum astutumque! Quisque placerat facilisis egestas cillum dolore. Nec dubitamus multa iter quae et nos invenerat.
+                    </p>
+                    <a href="#" class="button button-black">View Infographic</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
