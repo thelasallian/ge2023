4fadfb8f01ab57ea55600878b04f33cef9b1a2f1
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-11-04 18:32:38 +0800

    Added hidden coming soon buttons

--- php/homepage/h2h-fasttalk-preview.php
+++ php/homepage/h2h-fasttalk-preview.php
@@ -8,6 +8,7 @@
                 <h2 class="title">Head to Head</h2>
                 <p class="desc">Phasellus laoreet lorem vel dolor tempus vehicula. Idque Caesaris facere voluntate liceret: sese habere. Ab illo tempore, ab est sed immemorabili. Mercedem aut nummos unde unde extricat, amaras.</p>
                 <a href="head-to-head.php" class="button button-white">View Articles</a>
+                <!-- <a class="button button-white">Coming Soon</a> -->
             </div>
         </div>
         <div id="fastTalk" class="greenPreview">
@@ -18,6 +19,7 @@
                 <h2 class="title">Fast Talk</h2>
                 <p class="desc">Ab illo tempore, ab est sed immemorabili. Cum sociis natoque penatibus et magnis dis parturient. Quam diu etiam furor iste tuus nos eludet? Ut enim ad minim veniam, quis nostrud exercitation.</p>
                 <a href="fast-talk.php" class="button button-white">View Interactive</a>
+                <!-- <a class="button button-white">Coming Soon</a> -->
             </div>
         </div>
     </div>
