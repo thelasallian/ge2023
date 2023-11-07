22283ed5fa83127a722a02fef0b750a0260c6db1
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-11-07 20:26:06 +0800

    Added infographic label for how to vote

--- php/homepage/how-to-vote.php
+++ php/homepage/how-to-vote.php
@@ -4,6 +4,7 @@
             <div id="howToVote-container">
                 <img src="assets/homepage/how-to-vote.png" class="howToVote-img">
                 <div id="howToVote-info">
+                    <span class="tag tag-green">Infographic</span>
                     <h2 class="title">How to Vote</h2>
                     <p class="desc">
                         Fictum, deserunt mollit anim laborum astutumque! Quisque placerat facilisis egestas cillum dolore. Nec dubitamus multa iter quae et nos invenerat.
