2aab01148354a431645100c108efc6173c5b2986
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-11-11 09:10:45 +0800

    Updated vox pop info

--- php/vox-populi/header.php
+++ php/vox-populi/header.php
@@ -2,8 +2,8 @@
 // Text to display
 $id          = "vox-populi-header";
 $title       = "Vox Populi";
-$author_1    = "Michael Hamza Mustapha, Maxine Lee,";
-$author_2    = "Gilliane Luy";
+$author_1    = "Michael Hamza Mustapha, Gilliane Luy,";
+$author_2    = "Maxine Lee";
 $description = "To build a comprehensive narrative, working with different perspectives is often crucial. The LaSallian's photographers discovers that students envision the next chapter of DLSU's student leadership differently, entrusting it to the next USG officers to put everyone's hopes on the same page.";
 
 // Render HTML for the header
