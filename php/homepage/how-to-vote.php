c52ffe032513eeedf3ce7ab1ac84d71e085c28dd
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-11-12 09:04:11 +0800

    Content update mechanism

--- php/homepage/how-to-vote.php
+++ php/homepage/how-to-vote.php
@@ -9,8 +9,16 @@
                     <p class="desc">
                     DLSU Comelec confirmed that this year's elections will remain online. This infographic provides a step-by-step guide on how to lock in your vote.
                     </p>
-                    <!-- <a href="#" class="button button-black">View Infographic</a> -->
-                    <a class="button button-black" style="cursor: not-allowed;">Coming Soon</a>
+
+                    <?php 
+                    $contentUpdates = json_decode(file_get_contents('https://github.com/thelasallian/ge2023/blob/main/json/content-updates.json?raw=true'), true);
+                    ?>
+
+                    <?php if ($contentUpdates["how-to-vote"] == " "): ?>
+                        <a class="button button-black" style="cursor: not-allowed;">Coming Soon</a>
+                    <?php else: ?>
+                        <a href="#" class="button button-black">View Infographic</a>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
